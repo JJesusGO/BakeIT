8b217b45c25824e214e8e27ef594ade0e07d2fa0
TheReaperScythe <thereaperscythe@hotmail.com>
2021-06-16 16:45:34 -0500

    Actualizando: Estructura NodeJS+Express

--- servidor-bakeit/app.js
+++ servidor-bakeit/app.js
@@ -4,10 +4,28 @@ const app = express();
 const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 
+//CONFIGURACIÓN
+app.set("PUERTO",3000);
+
+//MIDDLEWARE
 app.use(express.static(publicPath));
 
-app.listen(3000, () => console.log('Servidor inicializado en localhost:3000')); 
-
+//RUTAS
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
-})
+});
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+app.get('/product-cart', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'));
+});
+app.get('/product-detail', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
+});
+
+const puerto = app.get('PUERTO') || 3000;
+app.listen(puerto, () => console.log('Servidor inicializado en localhost:'+puerto)); 
