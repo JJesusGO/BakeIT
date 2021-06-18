e2ffb7057aa6ef1e083a564d6e702d2cfc65dd64
TaniaTovar <43356807+TaniaTovar@users.noreply.github.com>
2021-06-17 22:56:27 -0500

    Primer maquetado de las paginas login y register, ya con sus respectivos archivos css

--- Servidor/app.js
+++ Servidor/app.js
@@ -6,8 +6,16 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => console.log('Servidor corriendo')); 
+app.listen(3000, () => console.log('Servidor corriendo'));
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
+})
+
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 })
