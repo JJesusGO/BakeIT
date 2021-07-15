7782dceaaa4a289ea2234b9feb8ac77d1b0e24f5
Tania <tania@MacBook-Air-de-Tania.local>
2021-07-14 19:20:06 -0500

    creacion jsons e instalacion multer

--- servidor-bakeit/src/controllers/productEditController.js
+++ servidor-bakeit/src/controllers/productEditController.js
@@ -1,27 +1,26 @@
-const { productos, agregarProducto, editarProducto} = require("../db/productos");
+const { productos, agregarProducto, editarProducto } = require("../data/productos");
 
 const controller = {
-    getProducts: (req, res) => {  
-        res.render('product/edit',{productos,edicion:-1});
+    getProducts: (req, res) => {
+        res.render('product/edit', { productos, edicion: -1 });
     },
     getProductsIndex: (req, res) => {
-        const {index} = req.params;
-        res.render('product/edit',{productos,edicion:index});
+        const { index } = req.params;
+        res.render('product/edit', { productos, edicion: index });
     },
-    postProducts: (req, res) => {        
-        const {nombre,edicion}= req.body;
-        let posicion="";
-        if(nombre!=undefined){
+    postProducts: (req, res) => {
+        const { nombre, edicion } = req.body;
+        let posicion = "";
+        if (nombre != undefined) {
             agregarProducto(req.body);
             posicion = "#";
-        }
-        else if(edicion!=undefined)
-            posicion = "/"+edicion+"#control";
-        res.redirect("/products-edit"+posicion);
+        } else if (edicion != undefined)
+            posicion = "/" + edicion + "#control";
+        res.redirect("/products-edit" + posicion);
     },
-    putProductsIndex: (req, res) => {        
-        const {index} = req.params;
-        editarProducto(parseInt(index),req.body);
+    putProductsIndex: (req, res) => {
+        const { index } = req.params;
+        editarProducto(parseInt(index), req.body);
         res.redirect("/products-edit");
     }
 };
