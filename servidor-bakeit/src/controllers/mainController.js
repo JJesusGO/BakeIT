cf7da532e73d97a5e5203a5427eaac86be9ec4a2
“MarcelaBrjs” <marcela.bg1@gmail.com>
2021-08-02 21:35:31 -0500

    Edición de rutas para administrar productos del JSON

--- servidor-bakeit/src/controllers/mainController.js
+++ servidor-bakeit/src/controllers/mainController.js
@@ -1,10 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const controlador = {
 
     getIndex: (req, res) => {
         res.render('index');
     },
     getGaleria: (req, res) => {
-        res.render('galeria');
+        res.render('galeria', { productos: products });
     }
 };
 
--- servidor-bakeit/src/controllers/productController.js
+++ servidor-bakeit/src/controllers/productController.js
@@ -1,10 +1,21 @@
 const uniqid = require('uniqid');
-const { productos, agregarProducto, editarProducto, eliminarProducto} = require("../data/products");
+const fs = require('fs');
+const path = require('path');
+
+const { productos, agregarProducto, editarProducto, eliminarProducto } = require("../data/products");
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const controlador = {
 
     getDetail: (req, res) => {
-        res.render('product/detail');
+        let productoSeleccionado;
+        for (let product of products) {
+            if (product.id == req.params.id) {
+                productoSeleccionado = product;
+            }
+        }
+        res.render('product/detail', { producto: productoSeleccionado });
     },
     getCart: (req, res) => {
         res.render('product/cart');
@@ -16,63 +27,63 @@ const controlador = {
         const { id } = req.params;
         const index = productos.findIndex(producto => producto.id === id);
         res.render('product/edit', { productos, edicion: index });
-    },    
-    postProduct: (req, res) => {   
-        const files = req.files;         
+    },
+    postProduct: (req, res) => {
+        const files = req.files;
         let elementos = [];
         let imagenes = [];
 
-        if(req.body.elementos!=undefined) 
+        if (req.body.elementos != undefined)
             elementos = req.body.elementos.split('\r\n');
-        for(let i=0;i<elementos.length;i++)
-            if((elementos[i].trim()).length <= 0)
-                elementos.splice(i--,1);
-        if(files!=undefined) 
-            imagenes = files.map(img=>{
-                return "/data/products/"+img.filename;
+        for (let i = 0; i < elementos.length; i++)
+            if ((elementos[i].trim()).length <= 0)
+                elementos.splice(i--, 1);
+        if (files != undefined)
+            imagenes = files.map(img => {
+                return "/data/products/" + img.filename;
             });
-        for(let i = 0; i < 4; i++) 
-            if(imagenes[i]==undefined)
+        for (let i = 0; i < 4; i++)
+            if (imagenes[i] == undefined)
                 imagenes[i] = "/media/img/placeholder.png";
-        
+
         const id = uniqid();
         const producto = {
             ...req.body,
             elementos,
             imagenes,
             id
-        }        
-        agregarProducto(producto);                    
+        }
+        agregarProducto(producto);
         res.redirect("/products/edit");
     },
     putProductID: (req, res) => {
         const { id } = req.params;
         const producto = productos.find(producto => producto.id === id);
 
-        const files = req.files;         
+        const files = req.files;
         let elementos = [];
         let imagenes = [];
 
-        if(req.body.elementos!=undefined) 
+        if (req.body.elementos != undefined)
             elementos = req.body.elementos.split('\r\n');
-        for(let i=0;i<elementos.length;i++)
-            if((elementos[i].trim()).length <= 0)
-                elementos.splice(i--,1);
-        if(files!=undefined) 
-            imagenes = files.map(img=>{
-                return "/data/products/"+img.filename;
+        for (let i = 0; i < elementos.length; i++)
+            if ((elementos[i].trim()).length <= 0)
+                elementos.splice(i--, 1);
+        if (files != undefined)
+            imagenes = files.map(img => {
+                return "/data/products/" + img.filename;
             });
-        for(let i = 0; i < 4; i++) 
-            if(imagenes[i]==undefined)
+        for (let i = 0; i < 4; i++)
+            if (imagenes[i] == undefined)
                 imagenes[i] = producto.imagenes[i];
-                
+
         const edicionproducto = {
             ...producto,
             ...req.body,
             elementos,
-            imagenes,            
-        }        
-        editarProducto(edicionproducto); 
+            imagenes,
+        }
+        editarProducto(edicionproducto);
         res.redirect("/products/edit");
     },
     deleteProductID: (req, res) => {
@@ -80,7 +91,7 @@ const controlador = {
         eliminarProducto(id);
         res.redirect("/products/edit");
     }
-    
+
 };
 
 module.exports = controlador;
