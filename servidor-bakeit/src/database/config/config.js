a75615440de578f2ace3fa3cdc4441d0a037bf1e
TheReaperScythe <thereaperscythe@hotmail.com>
2021-08-31 20:20:55 -0500

    Inicializando bases de datos con sequelize

---
+++ servidor-bakeit/src/database/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "development": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "port" : process.env.PORT,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "port" : process.env.PORT,
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "port" : process.env.PORT,
+    "dialect": "mysql"
+  }
+}
