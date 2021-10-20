9a704523358b8208fd49ca178ca8f827f1e607f4
Tania <tania.tovar95@gmail.com>
2021-10-20 01:24:28 -0500

    Cambio en categorias  Api de productos

--- servidor-bakeit/src/routes/api/productRoutes.js
+++ servidor-bakeit/src/routes/api/productRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const productAPIController = require('../../controllers/api/productController');
 
 router.get('/', productAPIController.products);
+router.get('/category', productAPIController.productsCategory);
 router.get('/:id', productAPIController.productsDetail);
 
 module.exports = router;
